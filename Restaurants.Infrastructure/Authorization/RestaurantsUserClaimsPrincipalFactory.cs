549a62d8e28498b799d5d982b72a250ce408e277
Marija Jovanovic <marijajovanovic671@gmail.com>
2025-02-02 12:31:43 +0100

    adding custom user claims

---
+++ Restaurants.Infrastructure/Authorization/RestaurantsUserClaimsPrincipalFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Authorization
+{
+    //klasa za dodavanje specificnih claim-ova
+    public class RestaurantsUserClaimsPrincipalFactory(UserManager<User> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IOptions<IdentityOptions> options) : UserClaimsPrincipalFactory<User, IdentityRole>(userManager, roleManager,options)
+    {
+        //kreiranje objekta ClaimsPrincipal koji sadrzi info o korisniku (id,role,podaci...)
+        //objekat sadrzi sve claim-ove korisnika
+        public override async Task<ClaimsPrincipal> CreateAsync(User user)
+        {
+            var id = await GenerateClaimsAsync(user); //generisanje osnovnih claim-ova
+            if (user.Nationality != null)
+            {
+                id.AddClaim(new Claim("Nationality", user.Nationality));
+            }
+            if (user.DateOfBirth != null)
+            {
+                id.AddClaim(new Claim("DateOfBirth", user.DateOfBirth.Value.ToString()));
+            }
+            return new ClaimsPrincipal(id);
+        }
+    }
+}
