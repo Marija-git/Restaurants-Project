eb1e999868e7eb4270e77961f39ae598f4238a96
Marija Jovanovic <marijajovanovic671@gmail.com>
2025-02-09 18:41:38 +0100

    testing api endpoints with authorization

--- Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -7,6 +7,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Domain.Repositories;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Moq;
+using Restaurants.API.Tests;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Restaurants.Domain.Entities;
+using Restaurants.Application.Restaurants.Dtos;
+using System.Net.Http.Json;
 
 namespace Restaurants.API.Controllers.Tests
 {
@@ -15,11 +25,61 @@ namespace Restaurants.API.Controllers.Tests
     {
         //omocuvanje pokretanja app unutar testa,pravljenje http clienta i slanje zahteva ka stvarnom serveru kao da je app u produkciji
         private readonly WebApplicationFactory<Program> _factory;
+        private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
 
         public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
         {
-            _factory = factory;
-        }       
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    //jedinstvena instanca za sve testove/za fejk auth
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>(); 
+
+                    //zamena postojeceg IRestaurantRepositor koji pristupa bazi sa mock/simulacijom tog repozitorija
+                    //koji vraca predefinisane rezultate
+                    //posebna instanca za svavki test
+                    services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                                                _ => _restaurantsRepositoryMock.Object));
+                });
+            });
+        }
+
+        [Fact]
+        public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+        {
+            //arrange
+            var id = 1111;
+            _restaurantsRepositoryMock.Setup(rep => rep.GetById(id)).ReturnsAsync((Restaurant?)null);
+            var client = _factory.CreateClient();
+
+            //act
+            var response = await client.GetAsync($"/api/restaurants/{id}");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetById_ForExistingId_ShouldReturn200OK()
+        {
+            //arrange
+            var restaurant = new Restaurant()
+            {
+                Id = 1111
+            };
+            _restaurantsRepositoryMock.Setup(rep => rep.GetById(restaurant.Id)).ReturnsAsync(restaurant);
+            var client = _factory.CreateClient();
+
+            //act
+            var response = await client.GetAsync($"/api/restaurants/{restaurant.Id}");
+            var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            restaurantDto.Should().NotBeNull();
+            restaurantDto.Id.Should().Be(restaurant.Id);
+        }
 
         [Fact]
         public async Task GetAllRestaurants_ValidRequest_Returns200OK()
---
+++ Restaurants.API.Tests/FakePolicyEvaluator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+
+namespace Restaurants.API.Tests
+{
+    internal class FakePolicyEvaluator : IPolicyEvaluator
+    {
+        //fejkamo autentifikaciju - metoda vraca uspesnu autentifikaciju bez provere
+        public Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+        {
+            var claimsPrincipal = new ClaimsPrincipal();
+
+            //pravimo random identity sa klejmovima 
+            claimsPrincipal.AddIdentity(new ClaimsIdentity( //kreiranje novog identiteta
+                new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "1"),
+                    new Claim(ClaimTypes.Role, "Admin"),
+                }));
+
+            var ticket = new AuthenticationTicket(claimsPrincipal, "Test"); //kreiranje tiketa sa info o korisniku
+            var result = AuthenticateResult.Success(ticket); //autentifikacija uspesno obavljena
+            return Task.FromResult(result);
+        }
+
+        //simuliranje uspesne autorizacije
+        public Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy, AuthenticateResult authenticationResult, HttpContext context, object? resource)
+        {
+            var result = PolicyAuthorizationResult.Success(); //return usepsan rezultat bez ikakve provere
+            return Task.FromResult(result);
+        }
+    }
+}
