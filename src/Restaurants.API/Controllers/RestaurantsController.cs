149d229deddc5e67c2df39b30505b9efcacb5669
Marija Jovanovic <marijajovanovic671@gmail.com>
2025-02-13 20:36:19 +0100

    working with blobs locally

--- src/Restaurants.API/Controllers/RestaurantsController.cs
+++ src/Restaurants.API/Controllers/RestaurantsController.cs
@@ -72,7 +72,7 @@ namespace Restaurants.API.Controllers
             var command = new UploadRestaurantLogoCommand()
             {
                 RestaurantId = id,
-                FileName = file.FileName,
+                FileName = $"{id}-{file.FileName}",
                 File = stream
 
             };
